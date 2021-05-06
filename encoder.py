f39dd86232d9eca3269db2909ee0f9955cf594cc
Gooner12 <kcbiaksh@gmail.com>
2021-05-06 10:22:06 +1000

    Deleted the old transformer for player's growth

--- encoder.py
+++ encoder.py
@@ -5,8 +5,11 @@ from pyspark.sql.functions import when, col
 # defining a custom transformer to perform label encoding for players' position group
 class LabelEncoder(Transformer):
     """
-    A custom Transformer which creates a target column based on the encoding of position group, such as
-    if position group is DEF, it is encoded as 1, 0 for FWD and 3 for MID
+    A custom Transformer which creates a target column based on the type of columns.
+    For column, position group, the encoding is performed in a way that if position group is DEF,
+    it is encoded as 1, 0 for FWD and 3 for MID. For column, Growth_Level, the encoding is done
+    in a way that if growth level is no growth, it is encoded as 0, 1 for mid growth and 3 for
+    high growth.
     """
 
     def __init__(self):
@@ -17,23 +20,9 @@ class LabelEncoder(Transformer):
         if('Position_Group' in df.columns):
             df = df.withColumn('Target',when(col('Position_Group') == 'DEF', 1).when(col('Position_Group')== 'FWD', 0)\
           .otherwise(2))
-        elif('Growth_Level in df.columns'):
+        elif('Growth_Level' in df.columns):
             df = df.withColumn('Target_Growth', when(col('Growth_Level') == 'No_Growth', 0).when(col('Growth_Level') == 'Mid_Growth', 1) \
                                .otherwise(2))
         return df
 
 
-# defining a custom transformer to perform label encoding for player growth
-class GrowthEncoder(Transformer):
-    """
-    A custom Transformer which creates a target column based on the encoding of players' growth level, such as
-    if growth level is no growth, it is encoded as 0, 1 for mid growth and 3 for high growth
-    """
-
-    def __init__(self):
-        super(GrowthEncoder, self).__init__()
-
-    def _transform(self, df: DataFrame) -> DataFrame:
-        df = df.withColumn('Target_Growth',when(col('Growth_Level') == 'No_Growth', 0).when(col('Growth_Level')== 'Mid_Growth', 1)\
-      .otherwise(2))
-        return df
